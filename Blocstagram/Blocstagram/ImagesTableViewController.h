5bb0fe6e2a159418d17c25f800731a8b25df900a
Nikko Sanchez <neeks08@gmail.com>
2016-08-09 15:25:28 -0700

    Checkpoint 27 complete

--- Blocstagram/Blocstagram/ImagesTableViewController.h
+++ Blocstagram/Blocstagram/ImagesTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  ImagesTableViewController.h
 //  Blocstagram
 //
 //  Created by Nikko on 8/9/16.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface ImagesTableViewController : UITableViewController
 
 @end
-
