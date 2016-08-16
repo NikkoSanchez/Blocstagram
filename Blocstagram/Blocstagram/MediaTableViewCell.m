66736ca85d89a4c1e33cbb1f421cf5a4d1ab5fac
Nikko Sanchez <neeks08@gmail.com>
2016-08-15 22:32:49 -0700

    checkpoint 34 completed

--- Blocstagram/Blocstagram/MediaTableViewCell.m
+++ Blocstagram/Blocstagram/MediaTableViewCell.m
@@ -104,8 +104,13 @@ static NSParagraphStyle *paragraphStyle;
     return mutableUsernameAndCaptionString;
 }
 
+- (void)setHighlighted:(BOOL)highlighted animated:(BOOL)animated {
+    [super setHighlighted:NO animated:animated];
+}
+
+
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
-    [super setSelected:selected animated:animated];
+    [super setSelected:NO animated:animated];
 
     // Configure the view for the selected state
 }
@@ -179,4 +184,6 @@ static NSParagraphStyle *paragraphStyle;
     
     return CGRectGetMaxY(layoutCell.commentLabel.frame);
 }
+
+
 @end
