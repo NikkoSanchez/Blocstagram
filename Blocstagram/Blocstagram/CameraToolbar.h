17bf99cccbec65a6713175aa0fb467a0d0968f76
Nikko Sanchez <neeks08@gmail.com>
2016-08-24 15:50:53 -0700

    checkpoint 41 complete

---
+++ Blocstagram/Blocstagram/CameraToolbar.h
@@ -0,0 +1,27 @@
+//
+//  CameraToolbar.h
+//  Blocstagram
+//
+//  Created by Nikko on 8/24/16.
+//  Copyright © 2016 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class CameraToolbar;
+
+@protocol CameraToolbarDelegate <NSObject>
+
+- (void) leftButtonPressedOnToolbar:(CameraToolbar *)toolbar;
+- (void) rightButtonPressedOnToolbar:(CameraToolbar *)toolbar;
+- (void) cameraButtonPressedOnToolbar:(CameraToolbar *)toolbar;
+
+@end
+
+@interface CameraToolbar : UIView
+
+- (instancetype) initWithImageNames:(NSArray *)imageNames;
+
+@property (nonatomic, weak) NSObject <CameraToolbarDelegate> *delegate;
+
+@end
---
+++ Blocstagram/Blocstagram/UIImage+ImageUtilities.h
@@ -0,0 +1,17 @@
+//
+//  UIImage+ImageUtilities.h
+//  Blocstagram
+//
+//  Created by Nikko on 8/24/16.
+//  Copyright © 2016 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (ImageUtilities)
+
+- (UIImage *) imageWithFixedOrientation;
+- (UIImage *) imageResizedToMatchAspectRatioOfSize:(CGSize)size;
+- (UIImage *) imageCroppedToRect:(CGRect)cropRect;
+
+@end
